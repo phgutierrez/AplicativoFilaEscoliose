f26174a84240ddf0424aaf772f838039ea826ea7
Pedro Freitas <pedrogvfreitas@gmail.com>
2025-04-19 23:03:08 -0300

    Mudanças para rodar em rede

--- run_app.bat
+++ run_app.bat
@@ -5,7 +5,7 @@ pip install -r requirements.txt --only-binary=numpy,pandas
 echo.
 echo ===== Iniciando aplicacao Flask =====
 set FLASK_APP=app.py
-set FLASK_ENV=development
+set FLASK_ENV=production
 python -m flask run --host=0.0.0.0 --port=5000
 
 pause
